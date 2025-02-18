27b347986775e1bdced67738a730b0889efc5e4b
Yorn90104 <bingxiao526@gmail.com>
2025-02-18 16:50:46 +0800

    addTodo

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-function CreateFrom() {
+function CreateFrom({addTodo}) {
 
     const [content, setContent] = useState()
+
     
     return (
         <>
-            <form className="create-form">
+            <form className="create-form" onSubmit={(e) => {addTodo(content); setContent(""); e.preventDefault();}}>
                 <input type="text" placeholder="輸入待辦事項" 
                 value={content} 
                 onChange={(e)=> setContent(e.target.value)}/>
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import CreateFrom from "./CreateForm";
 import Todo from "./Todo";
+
 function TodoWrapper() {
-    const [todos, setTodos] = useState(["打掃廁所", "寫作業"]);
+    const [todos, setTodos] = useState([       
+            {content:"打掃廁所", id : Math.random()},
+            {content:"寫作業", id : Math.random()} 
+        ]
+    );
+
+    const addTodo = (content) => {
+        setTodos([...todos, {content: content, id: Math.random()}]);
+    }
 
     return (
         <div className="wrapper">
             <h1>待辦事項</h1>
-            <CreateFrom />
+            <CreateFrom addTodo = {addTodo}/>
             {
                 todos.map((todo) => {
-                    return <Todo todo = {todo}/>
+                    return <Todo todo = {todo.content} key={todo.id}/>
                 })
             }
         </div>
